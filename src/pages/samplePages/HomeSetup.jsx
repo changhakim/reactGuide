7d8ca5b4c8150e6a40491f2d496b798332b9d43d
LAPTOP-6KUL3PT7\finger <is.yang@finger.co.kr>
2023-08-17 14:08:45 +0900

    설정 화면 수정

--- src/pages/samplePages/HomeSetup.jsx
+++ src/pages/samplePages/HomeSetup.jsx
@@ -1,104 +1,106 @@
 import { useState, useEffect } from "react";
-import { Toggle } from "@components";
-
 import Styles from "./Develop.module.scss";
-
+import { Toggle } from "@components";
+import { Tooltip } from "@components";
 function HomeSetup() {
   console.log("== HomeSetup ==");
-
   let [option1, setOption1] = useState(false);
+
+  const onNewsletterChange = (checked) => {
+    if (!checked) {
+      setOption1(false);
+    } else {
+      setOption1(true);
+    }
+  };
 
   return (
     <div className={Styles.page}>
-      <div className={Styles.tableContsPart}>
-        <div className={Styles.contsTitlePack}>
-          <h3 className={Styles.ebtitleNormal}>설정</h3>
-        </div>
-        <div className={Styles.tableViewPack}>
-          <table className={Styles.ebtable}>
-            <caption></caption>
-            <colgroup>
-              <col className={{ width: "240px" }} />
-              <col />
-            </colgroup>
-            <tbody>
-              <tr>
-                <th scope="row">홈 화면</th>
-                <td>
-                  <span>기본&gt;</span>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">큰 글씨</th>
-                <td>
-                  <Toggle
-                    id="option1"
-                    small
-                    disabled={false}
-                    checked={option1}
-                    onChange={setOption1}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">계좌순서</th>
-                <td>&gt;</td>
-              </tr>
-              <tr>
-                <th scope="row">메뉴 맞춤설정</th>
-                <td>&gt;</td>
-              </tr>
-            </tbody>
-          </table>
+      <div className={Styles.setList}>
+        <h1>홈설정</h1>
 
-          <table className={Styles.ebtable}>
-            <caption></caption>
-            <colgroup>
-              <col className={{ width: "240px" }} />
-              <col />
-            </colgroup>
-            <tbody>
-              <tr>
-                <th scope="row">자동 로그인</th>
-                <td>
-                  <Toggle
-                    id="option1"
-                    small
-                    disabled={false}
-                    checked={option1}
-                    onChange={setOption1}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">기본 로그인방법</th>
-                <td>간편비밀번호인증&gt;</td>
-              </tr>
-              <tr>
-                <th scope="row">인증/보안</th>
-                <td>&gt;</td>
-              </tr>
-            </tbody>
-          </table>
+        <ul>
+          <li>
+            홈화면 <span style={{ width: "149px" }}>기본&gt;</span>
+          </li>
+          <li>
+            큰글씨<span>&gt;</span>
+          </li>
+          <li>
+            계좌순서<span>&gt;</span>
+          </li>
+          <li>
+            메뉴맞춤설정<span>&gt;</span>
+          </li>
+        </ul>
+      </div>
 
-          <table className={Styles.ebtable}>
-            <caption></caption>
-            <colgroup>
-              <col className={{ width: "240px" }} />
-              <col />
-            </colgroup>
-            <tbody>
-              <tr>
-                <th scope="row">앱 알림</th>
-                <td>&gt;</td>
-              </tr>
-              <tr>
-                <th scope="row">문자 알림</th>
-                <td>&gt;</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div className={Styles.setList}>
+        <h1>로그인설정</h1>
+        <ul>
+          <li>
+            자동로그인{" "}
+            <Tooltip
+              content="간편인증으로 로그인한 경우에만 자동 로그인을 설정 할 수 있어요"
+              position="center"
+            >
+              ❓
+            </Tooltip>{" "}
+            <span style={{ width: "149px" }}>기본&gt;</span>
+          </li>
+          <li>
+            기존로그인방법
+            <span style={{ width: "247px" }}>간편비밀번호인증&gt;</span>
+          </li>
+          <li>
+            인증/보안<span>&gt;</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className={Styles.setList}>
+        <h1>알림 설정</h1>
+        <ul>
+          <li>
+            앱 알림 <span>&gt;</span>
+          </li>
+          <li>
+            문자 알림
+            <span>&gt;</span>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <span style={{ fontWeight: "bold" }}>앱 환경</span>
+      </div>
+      <ul className={Styles.mylist}>
+        <li>
+          앱 버전 <span style={{ float: "right" }}>2.99</span>
+        </li>
+        <li>
+          단말기 모바일번호<span style={{ float: "right" }}>LM-V500N</span>
+        </li>
+
+        <li>
+          단말기 OS버전<span style={{ float: "right" }}>12</span>
+        </li>
+      </ul>
+
+      <div className={Styles.setList}>
+        <ul>
+          <li>
+            클립보드 계좌번호 복사전용{" "}
+            <Tooltip content="클립보드 계좌번호 복사전용" position="center">
+              ❓
+            </Tooltip>{" "}
+            <span>&gt;</span>
+          </li>
+          <li>
+            캐시/쿠키 삭제
+            <span>&gt;</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
