905281153ab44b8c8efbb5a30c0f940dfea1a09c
LAPTOP-6KUL3PT7\finger <is.yang@finger.co.kr>
2023-08-18 16:40:11 +0900

    설정화면수정

--- src/pages/samplePages/HomeSetup.jsx
+++ src/pages/samplePages/HomeSetup.jsx
@@ -2,106 +2,171 @@ import { useState, useEffect } from "react";
 import Styles from "./Develop.module.scss";
 import { Toggle } from "@components";
 import { Tooltip } from "@components";
+
 function HomeSetup() {
   console.log("== HomeSetup ==");
   let [option1, setOption1] = useState(false);
+  let [option2, setOption2] = useState(false);
 
-  const onNewsletterChange = (checked) => {
-    if (!checked) {
-      setOption1(false);
-    } else {
-      setOption1(true);
-    }
-  };
+  function moveFuntion() {
+    alert("해당화면으로 이동합니다. (테스트)");
+  }
 
   return (
     <div className={Styles.page}>
-      <div className={Styles.setList}>
-        <h1>홈설정</h1>
-
-        <ul>
-          <li>
-            홈화면 <span style={{ width: "149px" }}>기본&gt;</span>
-          </li>
-          <li>
-            큰글씨<span>&gt;</span>
-          </li>
-          <li>
-            계좌순서<span>&gt;</span>
-          </li>
-          <li>
-            메뉴맞춤설정<span>&gt;</span>
-          </li>
-        </ul>
+      <div className={Styles.header}>
+        <h1>설정</h1>
       </div>
+      <div className={Styles.table01_area}>
+        <em>
+          <span style={{ fontWeight: "bold", color: "red" }}>홈설정</span>
+        </em>
+        <table className={Styles.table01}>
+          <tbody>
+            <tr>
+              <th scope="row">홈화면</th>
+              <td onClick={() => moveFuntion()}>기본&gt;</td>
+            </tr>
+            <tr>
+              <th scope="row">큰글씨</th>
+              <td>
+                {" "}
+                <Toggle
+                  id="option1"
+                  small
+                  disabled={false}
+                  checked={option1}
+                  onChange={setOption1}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">계좌순서</th>
+              <td onClick={() => moveFuntion()}>&gt;</td>
+            </tr>
+            <tr>
+              <th scope="row">메뉴맞춤설정</th>
+              <td onClick={() => moveFuntion()}>&gt;</td>
+            </tr>
+          </tbody>
+        </table>
 
-      <div className={Styles.setList}>
-        <h1>로그인설정</h1>
-        <ul>
-          <li>
-            자동로그인{" "}
-            <Tooltip
-              content="간편인증으로 로그인한 경우에만 자동 로그인을 설정 할 수 있어요"
-              position="center"
+        <em>
+          <span
+            style={{
+              fontWeight: "bold",
+              color: "red",
+              borderTop: "1px solid red",
+              margin: "30px 0px",
+            }}
+          >
+            로그인설정
+          </span>
+        </em>
+        <table className={Styles.table01}>
+          <tbody>
+            <tr>
+              <th scope="row">
+                자동로그인
+                <Tooltip content="자동로그인 설명" position="center">
+                  ❓
+                </Tooltip>{" "}
+              </th>
+              <td onClick={() => moveFuntion()}>기본&gt;</td>
+            </tr>
+            <tr>
+              <th scope="row">기본 로그인방법</th>
+              <td onClick={() => moveFuntion()}>간편비밀번호인증&gt;</td>
+            </tr>
+            <tr>
+              <th scope="row">인증/보안</th>
+              <td onClick={() => moveFuntion()}>&gt;</td>
+            </tr>
+            <tr>
+              <th scope="row">메뉴맞춤설정</th>
+              <td onClick={() => moveFuntion()}>&gt;</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <em>
+          <span
+            style={{
+              fontWeight: "bold",
+              color: "red",
+              borderTop: "1px solid red",
+              margin: "30px 0px",
+            }}
+          >
+            알림설정
+          </span>
+        </em>
+        <table className={Styles.table01}>
+          <tbody>
+            <tr>
+              <th scope="row">앱 알림</th>
+              <td onClick={() => moveFuntion()}>&gt;</td>
+            </tr>
+            <tr>
+              <th scope="row">문자 알림</th>
+              <td onClick={() => moveFuntion()}>&gt;</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div>
+          <em>
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "red",
+                borderTop: "1px solid red",
+                margin: "30px 0px",
+              }}
             >
-              ❓
-            </Tooltip>{" "}
-            <span style={{ width: "149px" }}>기본&gt;</span>
+              앱 환경
+            </span>
+          </em>
+        </div>
+        <ul className={Styles.mylist}>
+          <li>
+            앱 버전 <span style={{ float: "right" }}>2.99</span>
           </li>
           <li>
-            기존로그인방법
-            <span style={{ width: "247px" }}>간편비밀번호인증&gt;</span>
+            단말기 모바일번호<span style={{ float: "right" }}>LM-V500N</span>
           </li>
+
           <li>
-            인증/보안<span>&gt;</span>
+            단말기 OS버전<span style={{ float: "right" }}>12</span>
           </li>
         </ul>
       </div>
 
-      <div className={Styles.setList}>
-        <h1>알림 설정</h1>
-        <ul>
-          <li>
-            앱 알림 <span>&gt;</span>
-          </li>
-          <li>
-            문자 알림
-            <span>&gt;</span>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <span style={{ fontWeight: "bold" }}>앱 환경</span>
-      </div>
-      <ul className={Styles.mylist}>
-        <li>
-          앱 버전 <span style={{ float: "right" }}>2.99</span>
-        </li>
-        <li>
-          단말기 모바일번호<span style={{ float: "right" }}>LM-V500N</span>
-        </li>
-
-        <li>
-          단말기 OS버전<span style={{ float: "right" }}>12</span>
-        </li>
-      </ul>
-
-      <div className={Styles.setList}>
-        <ul>
-          <li>
-            클립보드 계좌번호 복사전용{" "}
-            <Tooltip content="클립보드 계좌번호 복사전용" position="center">
-              ❓
-            </Tooltip>{" "}
-            <span>&gt;</span>
-          </li>
-          <li>
-            캐시/쿠키 삭제
-            <span>&gt;</span>
-          </li>
-        </ul>
-      </div>
+      <table className={Styles.table01}>
+        <tbody>
+          <tr>
+            <th scope="row">
+              클립보드 계좌번호 복사허용
+              <Tooltip content="클립보드 복사허용" position="center">
+                ❓
+              </Tooltip>{" "}
+            </th>
+            <td>
+              <Toggle
+                id="option2"
+                small
+                disabled={false}
+                checked={option2}
+                onChange={setOption2}
+              />
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">캐시/쿠키삭제</th>
+            <td onClick={() => moveFuntion()}>&gt;</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
